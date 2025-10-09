5367508e756ee8cbb839d2f3705c6520c8297eff
Harley Cruz <johnharleycruz592@gmail.com>
2025-10-09 14:14:42 +0800

    Organizer page

--- accounts/urls.py
+++ accounts/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path("home/", views.home_view, name="home"),
     path("logout/", views.logout_view, name="logout"),
     path("confirm_logout/", views.confirm_logout_view, name="confirm_logout"),
+    path("organizer/", views.organizer_view, name="organizer"),
 ]
