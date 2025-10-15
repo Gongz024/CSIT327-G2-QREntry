7a7f9392e1cb1bf550122b8a7d8f1b9dc9b11ec8
Rocky <Rocky1.decretales@gmail.com>
2025-10-15 12:18:26 +0800

    Finished the Forgot password features

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,6 @@
-from django.urls import path
+from django.urls import path, reverse_lazy
 from . import views
+from django.contrib.auth import views as auth_views
 
 app_name = "accounts"
 
@@ -13,4 +14,40 @@ urlpatterns = [
 
     path('organizer/create-event/', views.create_event_view, name='create_event'),
     path('event/', views.view_events_view, name='event'),
+
+    # Forgot Password
+    path(
+        "password_reset/",
+    auth_views.PasswordResetView.as_view(
+        template_name="accounts/password_reset.html",
+        email_template_name="accounts/password_reset_email.html",
+        subject_template_name="accounts/password_reset_subject.txt",
+        extra_email_context={"brand_name": "Event CIT"},
+        from_email="Event CIT <noreply.eventcit@gmail.com>",  # Optional custom sender name
+        success_url="/accounts/password_reset_done/",
+    ),
+    name="password_reset",
+),
+    path(
+        "password_reset_done/",
+        auth_views.PasswordResetDoneView.as_view(
+            template_name="accounts/password_reset_done.html"
+        ),
+        name="password_reset_done",
+    ),
+    path(
+        "reset/<uidb64>/<token>/",
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name="accounts/password_reset_confirm.html",
+            success_url=reverse_lazy("accounts:password_reset_complete"),
+        ),
+        name="password_reset_confirm",
+    ),
+    path(
+        "reset/done/",
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name="accounts/password_reset_complete.html"
+        ),
+        name="password_reset_complete",
+    ),
 ]
--- qreentry_project/urls.py
+++ qreentry_project/urls.py
@@ -5,6 +5,6 @@ from django.shortcuts import redirect
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("accounts/", include("accounts.urls")),
+    path("accounts/", include("accounts.urls", namespace="accounts")),
     path("", lambda request: redirect("accounts:login")),
 ]
