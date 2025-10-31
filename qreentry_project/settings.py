db752a6bd5a3b72aa4bdb707d349d4c4b98d4c91
Drei Nash <dinaponash26@gmail.com>
2025-10-31 18:31:09 +0800

    Added a Bookmark UI

--- qreentry_project/settings.py
+++ qreentry_project/settings.py
@@ -29,8 +29,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',  # ✅ Keep for serving static files
+    "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
