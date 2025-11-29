9cd7f7cf53d990ff20a4c677e5a680dc60c9282b
Harley Cruz <johnharleycruz592@gmail.com>
2025-11-30 00:46:55 +0800

    improvised tikcet owned, its indicators, event.html infomration, edit event improvised its date format, etc.

--- accounts/urls.py
+++ accounts/urls.py
@@ -27,6 +27,7 @@ urlpatterns = [
     path('my-tickets/', views.ticket_owned_view, name='ticket_owned'),
     path('delete-ticket/<int:ticket_id>/', views.delete_ticket_view, name='delete_ticket'),
     path("live-search-organizer-events/", views.live_search_organizer_events, name="live_search_organizer_events"),
+    path('logout/organizer/confirm/', views.confirmlogout_organizer_view, name='confirmlogout_organizer'),
 
     path('event/<int:event_id>/bookmark/confirmation/', views.confirmation_bookmark_view, name='confirmation_bookmark'),
 
